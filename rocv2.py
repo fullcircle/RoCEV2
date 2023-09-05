ca2ae8af60b5e8f09cb79c6cf628821042a3b540
fullcircle <kevin@fullcirclenetworks.com>
2023-09-05 20:22:19 +0000

    added weighted red
    in addition to modeling loss I've added WRED thresholds to show the effects of congestion control.

--- rocv2.py
+++ rocv2.py
@@ -1,41 +1,52 @@
 import simpy
 import random
 
-
 # Define RoCEv2 packet types
 class RoCEv2Packet:
+    def __init__(self, packet_type, payload):
+        self.packet_type = packet_type
+        self.payload = payload
 
-  def __init__(self, packet_type, payload):
-    self.packet_type = packet_type
-    self.payload = payload
+# Define a simple WRED class
+class WRED:
+    def __init__(self, min_threshold, max_threshold, max_probability):
+        self.min_threshold = min_threshold
+        self.max_threshold = max_threshold
+        self.max_probability = max_probability
 
+    def drop_packet(self, queue_size):
+        if queue_size <= self.min_threshold:
+            return False
+        elif queue_size >= self.max_threshold:
+            return True
+        else:
+            probability = (queue_size - self.min_threshold) / (self.max_threshold - self.min_threshold)
+            return random.random() < probability * self.max_probability
 
 def rocev2_sender(env, receiver):
-  while True:
-    # Simulate sending RoCEv2 packets
-    packet_type = random.choice(["Data", "Ack", "Error"])
-    payload = f"Data Payload" if packet_type == "Data" else None
-    packet = RoCEv2Packet(packet_type, payload)
-    print(f"Sending {packet.packet_type} packet")
-    receiver.put(packet)  # Send packet to the receiver
-    yield env.timeout(1)  # Adjust the time interval for traffic generation
+    while True:
+        # Simulate sending RoCEv2 packets
+        packet_type = random.choice(["Data", "Ack", "Error"])
+        payload = f"Data Payload" if packet_type == "Data" else None
+        packet = RoCEv2Packet(packet_type, payload)
+        print(f"Sending {packet.packet_type} packet")
+        receiver.put(packet)  # Send packet to the receiver
+        yield env.timeout(1)  # Adjust the time interval for traffic generation
 
+def rocev2_receiver(env, switch, wred):
+    while True:
+        # Receive and process RoCEv2 packets
+        packet = yield switch.get()
+        print(f"Received {packet.packet_type} packet")
 
-def rocev2_receiver(env):
-  while True:
-    # Receive and process RoCEv2 packets
-    packet = yield receiver.get()
-    print(f"Received {packet.packet_type} packet")
-
-    if packet.packet_type == "Error":
-      # Simulate error handling
-      print("Error detected. Initiating error recovery...")
-      yield env.timeout(5)  # Simulate error recovery time
-      print("Error recovery complete.")
-    else:
-      # Process normal packets
-      yield env.timeout(1)  # Adjust processing time
-
+        if packet.packet_type == "Error":
+            # Simulate error handling
+            print("Error detected. Initiating error recovery...")
+            yield env.timeout(5)  # Simulate error recovery time
+            print("Error recovery complete.")
+        else:
+            # Process normal packets
+            yield env.timeout(1)  # Adjust processing time
 
 # Create a SimPy environment
 env = simpy.Environment()
@@ -43,19 +54,11 @@ env = simpy.Environment()
 # Create a shared event for sender and receiver
 receiver = simpy.Store(env)
 
-# Create RoCEv2 sender and receiver processes
+# Create RoCEv2 sender process
 env.process(rocev2_sender(env, receiver))
-env.process(rocev2_receiver(env))
+
+# Create RoCEv2 receiver process
+env.process(rocev2_receiver(env, receiver, None))  # Note: WRED not used in receiver
 
 # Run the simulation for a specified duration
-env.run(until=2000)  # Adjust the simulation duration as needed
-
-"""In this extended simulation:
-
-    We've introduced a RoCEv2Packet class to represent RoCEv2 packets with different types (Data, Ack, Error).
-
-    The rocev2_sender process now randomly selects packet types, including "Error" packets, and sends them to the receiver. When an "Error" packet is sent, it simulates an error condition.
-
-    The rocev2_receiver process detects "Error" packets and initiates error recovery, which takes 5 time units in this example. During this time, no normal packet processing occurs.
-
-You can further refine error handling by implementing specific error recovery mechanisms for RoCEv2, such as retransmissions or error correction codes, depending on your simulation objectives. This example provides a foundation for introducing error scenarios and observing how RoCEv2 handles them within your simulation."""
+env.run(until=20)  # Adjust the simulation duration as needed
